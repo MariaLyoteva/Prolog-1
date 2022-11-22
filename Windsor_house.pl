1f8e17cedf5a89fb16e76a82cff96832142b7544
MariaLyoteva <71443789+MariaLyoteva@users.noreply.github.com>
2022-11-22 11:16:20 +0100

    Project
    Contains only males and females without their relations.

---
+++ Windsor_house.pl
@@ -0,0 +1,105 @@
+% males of generation 1
+male(albert_prince).
+
+%females of generation 1
+female(victoria_queen).
+
+%males generation 2
+male(edward_king).
+male(alfred).
+
+%females of gen2
+female(victoria).
+female(alexandra).
+female(alice).
+female(helena).
+
+
+%males of generation 3
+male(georgeV_King).
+
+%females gen3
+female(mary).
+
+
+%males of generation 4
+male(edwardVIII_King).
+male(georgeVII_King).
+male(henry_Lascelles).
+male(henry).
+male(george_DukeOfKent).
+male(john).
+
+%females gen4
+female(elizabeth_Lyon).
+female(mary_gen4).
+female(alice).
+female(marina).
+
+%males of generation 5
+male(philip_Prince).
+male(anthony_Armstrong_Jones).
+male(richard).
+male(edward_DukeOfKent).
+male(angus).
+male(michael_PrinceOfKent).
+
+%females gen5
+female(elizabeth_2).
+female(margareth).
+female(biraitte).
+female(katherine).
+female(alexandra_Kent).
+female(michelle).
+
+%males gen 6
+male(charles).
+male(andrew).
+male(frederick).
+male(mark).
+male(timothy).
+
+
+%females gen6
+female(anne).
+female(gabriella).
+female(dianna).
+female(camilla).
+female(sarah).
+female(sophie).
+
+%males gen 7
+male(william).
+male(harry).
+male(peter).
+male(mike).
+male(james).
+
+%females gen7
+female(catherine).
+female(meghan).
+female(beatrice).
+female(eugenie).
+female(louise).
+female(zara).
+female(autumn).
+
+%males gen8
+male(george_gen8).
+male(louis).
+male(archie).
+
+%females gen8
+female(charlotte).
+female(savanah).
+female(isla).
+female(mia).
+female(lena).
+female(lilibeth).
+
+
+
+
+
+
+
